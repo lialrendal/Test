cd7acf5c6b3e34238a3f71495be7c739982b242b
Burst_V2 <megallamarada@gmail.com>
2021-04-20 18:48:18 +0200

    Added "Even-Odd Check"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int X = 75;
+        int Y = 22;
+        if (X%2 == 0)  {
+            System.out.println("This number is even.");
+        }
+        else {
+            System.out.println(X + " is an odd number");
+        }
     }
 }
