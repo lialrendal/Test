53e596c41b8b227b2e643d313247463c6c6c04ed
Burst_V2 <megallamarada@gmail.com>
2021-04-22 04:37:28 +0200

    Added "Even-Odd Check"

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,75 @@
+
+
 public class Main {
     public static void main(String[] args) {
+
+        //Even-Odd Check
+
         int X = 75;
-        int Y = 22;
         if (X%2 == 0)  {
             System.out.println("This number is even.");
         }
         else {
             System.out.println(X + " is an odd number");
         }
+
+        // Factorial calculator
+
+        int num_factorial = 7;
+        int result = 1;
+
+        for (int i = 1; i <= num_factorial; i++) {
+            result = result * i;
+        }
+        System.out.println(result);
+
+
+        // Prime number checker
+
+        int e;
+        boolean invalid = false;
+        for (e = 1; e <= 100; e++) {
+            for (int j = 2; j < e; j++) {
+                if (e % j == 0)  {
+                    invalid = true;
+                }
+            }
+            if (invalid == false) {
+                System.out.println(e + " is a prime number.");
+            }
+            invalid = false;
+        }
+
+
+
+        //Reverse string (+ palyndrome)
+
+
+
+        // Switch case
+
+        char c = 'a';
+
+        switch (c)  {
+            case 'a':
+                System.out.println( "This character is a vowel.");
+                break;
+            case 'e':
+                System.out.println( "This character is a vowel.");
+                break;
+            case 'i':
+                System.out.println( "This character is a vowel.");
+                break;
+            case 'o':
+                System.out.println( "This character is a vowel.");
+                break;
+            case 'u':
+                System.out.println( "This character is a vowel.");
+                break;
+            default:
+                System.out.println( "This character is a consonant.");
+        }
+
+
     }
 }
