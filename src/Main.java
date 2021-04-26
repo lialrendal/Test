902af1458fc464466c8cca1e0fa22d2be08f9637
Burst_V2 <megallamarada@gmail.com>
2021-04-26 18:23:27 +0200

    - Exercise 1 finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 
 public class Main {
 
+
+    public static int getParteEntera(Double number) {
+        String numStr = number.toString();
+        String[] array = numStr.split("[.]");
+        Integer result = Integer.parseInt(array[0]);
+        return result;
+    }
+
+    public static int getParteDecimal(Double number) {
+        String numStr = number.toString();
+        String[] array = numStr.split("[.]");
+        Integer result = Integer.parseInt(array[1]);
+        return result;
+    }
+
+
+
     public static void main(String[] args) {
+        System.out.println(getParteEntera(115.2387441));
+        System.out.println(getParteDecimal(115.2387441));
+
+
     }
 }
