8e2a8dabc0a49970f15a2122f4517aa30ce38b48
Burst_V2 <megallamarada@gmail.com>
2021-04-27 17:36:38 +0200

    - Exercise 2 finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 
 public class Main {
 
+    public static void ullmanConj (int num) {
+        System.out.println(num);
+        while (num > 1) {
+            if (num % 2 == 0) {
+                num = num/2;
+            }
+            else {
+                num = num*3 + 1;
+            }
+            System.out.println(num);
+
+        }
+    }
+
     public static void main(String[] args) {
+        ullmanConj(78);
     }
 }
