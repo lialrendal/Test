014469fcecfc52f74fc15155654142c9497fcabf
Burst_V2 <megallamarada@gmail.com>
2021-04-28 04:52:25 +0200

    - Exercise 3 finished (only lowercase results)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static StringBuilder cifradoCesar (java.lang.String cadenaACifrar)  {
+        int i = 0;
+        cadenaACifrar = cadenaACifrar.toLowerCase();
+        StringBuilder result = new StringBuilder(cadenaACifrar);
+        while (i < cadenaACifrar.length())  {
+            char cipher = cadenaACifrar.charAt(i);
+            int change = cipher;
+            change = change + 3;
+            if (change > 122)  {
+                change = change - 26;
+            }
+            result.setCharAt(i, (char) change);
+            i = i + 1;
+        }
+        return result;
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(cifradoCesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+    }
+
 }
